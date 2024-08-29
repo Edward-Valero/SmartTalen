acefe61a3101dcc3bf41ac6cb6a235e15ccbbc0d
Edward-Valero <valeroe6@gmail.com>
2024-08-28 21:06:55 -0500

    Create webpack.config.js

---
+++ shell/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'shell',
+      filename: 'remoteEntry.js',
+      remotes: {
+        userAdmin: 'userAdmin@http://localhost:3001/remoteEntry.js',
+        taskManagement: 'taskManagement@http://localhost:3002/remoteEntry.js',
+        reports: 'reports@http://localhost:3003/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
