067a5e9a6c9b01a648b9d01fffd7d6a26593266d
Edward-Valero <valeroe6@gmail.com>
2024-08-28 21:05:53 -0500

    Create taskSlice.js

---
+++ task-management/src/store/taskSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+    removeTask: (state, action) => {
+      return state.filter(task => task.id !== action.payload);
+    },
+    updateTask: (state, action) => {
+      const index = state.findIndex(task => task.id === action.payload.id);
+      if (index !== -1) {
+        state[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { addTask, removeTask, updateTask } = taskSlice.actions;
+export default taskSlice.reducer;
